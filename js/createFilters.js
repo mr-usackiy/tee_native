a1f338d9962a9f00439e1a3113f272460c616a98
Niko1248 <Sirius04@bk.ru>
2022-09-12 22:25:45 +0300

    The filter is ready The filter is ready

--- js/createFilters.js
+++ js/createFilters.js
@@ -1,47 +1,26 @@
-// const product = []
-// function uniqueCountries() {
-// 	let prr = new Set();
-// 	getProducts.then(render =>{	//убрать undefined из множества
-// 		render.forEach(element => {
-// 				prr.add(element.country)
-// 				product.push(element)
-// 		});
-// 	})
-// 	console.log(prr);
-// }	
-// uniqueCountries();
-
-
-	
-// }
-// createFilters(prr);
-// function filterCountry(setCountries){
-// 	for(item of setCountries){
-// 		console.log(item);
-// 		const filterCountryHTML = 
-// 			`<label>
-// 				${item}
-// 				<input type="checkbox" name="${item}" value="${item}">
-// 			</label>`
-// 		filters.insertAdjacentHTML('beforeend', filterCountryHTML)
-// 	}
-// }
-
-const blockFilters = document.querySelectorAll('.filter');
-blockFilters.forEach(element => {
-	element.addEventListener('click', function (event){
+filterList = new Set() // Создаем множество
+counterFilter = 0
+const blockFilters = document.querySelectorAll('.filter'); // Забираем все фильтры
+blockFilters.forEach(element => {					
+	element.addEventListener('click', function (event){				//Вешаем слушатели на все фильтры
 		const listProducts = productsContainer.children;
-		for (const item of listProducts) {
-			if (item.dataset.country !== event.target.value){
-				item.classList.toggle('blocked');
+		for (const item of listProducts){												//Блокируем все продукты при нажатии на фильтр
+			item.classList.add('blocked')
+		}
+		for (const item of listProducts) {											// Если значение нажатого фильтра = стране на карточке и кнопка нажата то добавляем в множество
+			if(event.target.value == item.dataset.country && event.target.checked){
+				filterList.add(item);
+			}else if(event.target.value == item.dataset.country && event.target.checked == false){ // Если не нажата то убираем из множества
+				filterList.delete(item);
+			}
+		}
+		filterList.forEach( el =>{																//Проходим по множеству и убираем блокировку продуктов
+			el.classList.remove('blocked')
+		})
+		if(filterList.size == 0){
+			for (const item of listProducts){												// При пустом множестве возвращаем все продукты
+				item.classList.remove('blocked')
 			}
 		}
 	})
 });
-
-
-// item.classList.add('blocked');
-// 		else{
-// 			item.classList.remove('blocked')
-// 		}	
-	
