121bf27d8bf0c3e0b0b90099344252d9dfab1335
Niko1248 <Sirius04@bk.ru>
2022-09-15 21:54:41 +0300

    add button for filters

--- js/createFilters.js
+++ js/createFilters.js
@@ -1,26 +1,49 @@
-filterList = new Set() // Создаем множество
-counterFilter = 0
+productsAfterFilters = new Set() // Создаем множество храняее продукты после фильтров
+titleFilters = new Set();		// Множество хранящее заголовок фильтра
 const blockFilters = document.querySelectorAll('.filter'); // Забираем все фильтры
-blockFilters.forEach(element => {					
-	element.addEventListener('click', function (event){				//Вешаем слушатели на все фильтры
-		const listProducts = productsContainer.children;
-		for (const item of listProducts){												//Блокируем все продукты при нажатии на фильтр
-			item.classList.add('blocked')
-		}
-		for (const item of listProducts) {											// Если значение нажатого фильтра = стране на карточке и кнопка нажата то добавляем в множество
-			if(event.target.value == item.dataset.country && event.target.checked){
-				filterList.add(item);
-			}else if(event.target.value == item.dataset.country && event.target.checked == false){ // Если не нажата то убираем из множества
-				filterList.delete(item);
+const filterBtn = document.querySelector('.filter__btn')
+const listProducts = productsContainer.children;
+
+
+console.log(listProducts);
+for (const goods of listProducts) {
+	console.log(goods)
+}
+
+
+
+
+function setFilters() {
+	blockFilters.forEach(element => {					
+		element.addEventListener('click', (event) => {				//Вешаем слушатели на все фильтры
+			for (const item of listProducts) {											// Если значение нажатого фильтра = стране на карточке и кнопка нажата то добавляем в множество
+				if(event.target.value == item.dataset.country && event.target.checked){
+					productsAfterFilters.add(item);
+				}else if(event.target.value == item.dataset.country && event.target.checked == false){ // Если не нажата то убираем из множества
+					productsAfterFilters.delete(item);
+				}
 			}
-		}
-		filterList.forEach( el =>{																//Проходим по множеству и убираем блокировку продуктов
-			el.classList.remove('blocked')
 		})
-		if(filterList.size == 0){
-			for (const item of listProducts){												// При пустом множестве возвращаем все продукты
-				item.classList.remove('blocked')
+	});
+}
+function applyFilters() {
+	filterBtn.addEventListener('click', (el) => {
+		if(el){
+			el.preventDefault();
+			for (const item of listProducts){												//Блокируем все продукты при нажатии на кнопку
+				item.classList.add('blocked');
+			}
+			productsAfterFilters.forEach( el =>{																//Проходим по множеству и убираем блокировку продуктов
+				el.classList.remove('blocked');
+			})
+			if(productsAfterFilters.size == 0){
+				for (const item of listProducts){												// При пустом множестве возвращаем все продукты
+					item.classList.remove('blocked');
+				}
 			}
 		}
 	})
-});
+}
+
+setFilters();
+applyFilters(productsAfterFilters);
